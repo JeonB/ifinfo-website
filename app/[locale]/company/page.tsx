1e3de8023836649528377e380173048298967518
JeonB <newjeonb@gmail.com>
2024-09-01 17:09:24 +0900

    company 페이지 내용변경

--- app/[locale]/company/page.tsx
+++ app/[locale]/company/page.tsx
@@ -1,9 +1,38 @@
-export default function About() {
+'use client'
+import FadeInview from '@/components/common/utill/FadeInview'
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import 'react-multi-carousel/lib/styles.css'
+import './card-styles.css'
+export default function Company() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Company Name was founded in...</p>
-      <p>Our mission is to...</p>
-    </div>
+    <FadeInview type="scale" range={0.3}>
+      <Flex
+        justify="center"
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+        }}>
+        <Box style={{ width: '100%', marginBottom: '3em' }}>
+          <Heading
+            className="mainSection1text"
+            size="3xl"
+            style={{
+              fontStyle: 'italic',
+              textAlign: 'center',
+            }}>
+            Company
+          </Heading>
+          <Heading
+            className="mainSection1text"
+            size="lg"
+            style={{
+              textAlign: 'center',
+              fontWeight: '400',
+              marginTop: 10,
+            }}></Heading>
+        </Box>
+      </Flex>
+    </FadeInview>
   )
 }
