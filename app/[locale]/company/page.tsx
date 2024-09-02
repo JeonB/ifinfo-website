bbdc81f5c84be2560395bedb46a8acc60696130e
JeonB <newjeonb@gmail.com>
2024-09-02 17:17:11 +0900

    안 쓰는 의존성 삭제

--- app/[locale]/company/page.tsx
+++ app/[locale]/company/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 import FadeInview from '@/components/common/utill/FadeInview'
 import { Box, Flex, Heading } from '@chakra-ui/react'
-import 'react-multi-carousel/lib/styles.css'
-import './card-styles.css'
 export default function Company() {
   return (
     <FadeInview type="scale" range={0.3}>
