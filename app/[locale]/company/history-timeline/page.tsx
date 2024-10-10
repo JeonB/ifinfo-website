b14bbe6a40d4f34908553bea08626f01dc8144bb
JeonB <newjeonb@gmail.com>
2024-10-10 21:04:45 +0900

    연혁 데이터 로딩테스트 3

--- app/[locale]/company/history-timeline/page.tsx
+++ app/[locale]/company/history-timeline/page.tsx
@@ -1,23 +1,62 @@
+'use client'
+
+import { Box, Spinner } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import TimelineComponent from './TimelineComponent'
 
-export default async function HistoryPage() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
-  const response = await fetch(`${baseUrl}/api/timeline`) // ISR을 위해 데이터 비캐싱 처리
-  const timelineData = await response.json()
+export default function HistoryPage() {
+  const [timelineData, setTimelineData] = useState<
+    { year: number; events: { date: string; description: string }[] }[]
+  >([])
+  const [loading, setLoading] = useState(true)
 
-  const sortedData = timelineData.sort(
-    (a: { year: number }, b: { year: number }) => b.year - a.year,
-  )
+  const fetchData = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch('/api/timeline') // 클라이언트 사이드에서 패칭
+      if (response.ok) {
+        const data = await response.json()
 
-  sortedData.forEach(
-    (yearData: { events: { date: string; description: string }[] }) => {
-      yearData.events.sort((a, b) => b.date.localeCompare(a.date))
-    },
-  )
+        const sortedData = data.sort(
+          (a: { year: number }, b: { year: number }) => b.year - a.year,
+        )
+
+        sortedData.forEach(
+          (yearData: { events: { date: string; description: string }[] }) => {
+            yearData.events.sort((a, b) => b.date.localeCompare(a.date))
+          },
+        )
+
+        setTimelineData(sortedData)
+      } else {
+        console.error('Failed to fetch data:', response.statusText)
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    } finally {
+      setLoading(false) // 로딩 상태 종료
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div>
-      <TimelineComponent data={sortedData} />
+      {loading ? (
+        // 로딩 중일 때 스피너를 표시
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minH="100vh">
+          <Spinner size="xl" />
+        </Box>
+      ) : (
+        // 데이터 로드가 완료되면 타임라인을 표시
+        <TimelineComponent data={timelineData} />
+      )}
     </div>
   )
 }
